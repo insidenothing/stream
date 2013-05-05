d214a2d8926d8947841fec995ddf79cd2f37ac84
Patrick McGuire <insidenothing@gmail.com>
2013-05-04 21:18:08 -0400

    template set

---
+++ application/views/common_footer.php
@@ -0,0 +1,123 @@
+
+<div style='clear:both;'></div>
+<div id='footer-widgets-container'>
+<div class='clearfix' id='footer-widgets'>
+<div class='footer-widget-box'>
+<ul class='widget-container'>
+<li>
+<div class='footersec section' id='footersec1'><div class='widget HTML' id='HTML4'>
+<h2 class='title'>Stream news</h2>
+<div class='widget-content'>
+<script type="text/javascript">if(typeof jQuery == 'undefined'){document.write("<scr" + "ipt type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js'></scr" + "ipt>");}</script>
+<ul id="rssdata1" style="display:none;"></ul><div id="rssloading1">Loading...</div>
+<script type="text/javascript">
+
+jQuery.noConflict();
+
+if( ! jQuery().jGFeed ) { (function(jQuery){jQuery.extend({jGFeed:function(url,fnk,num,key){if(url==null){return false;}var gurl="http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&q="+url;if(num!=null){gurl+="&num="+num;}if(key!=null){gurl+="&key="+key;}jQuery.getJSON(gurl,function(data){if(typeof fnk=="function"){fnk.call(this,data.responseData.feed);}else{return false;}});}});})(jQuery); }
+jQuery.jGFeed('http://blog.btemplates.com/feed/',
+function(feeds){
+if(!feeds){return false;}
+for (var i=0; i < feeds.entries.length; i++) {
+	var item = feeds.entries[i];
+	var item_html = '<li><a href="'+item.link+'">'+item.title+'</a></li>';
+	jQuery('#rssdata1').append(item_html);
+}
+jQuery('#rssloading1').fadeOut();
+jQuery('#rssdata1').slideDown();
+}, 5);</script>
+</div>
+<div class='clear'></div>
+<span class='widget-item-control'>
+<span class='item-control blog-admin'>
+<a class='quickedit' href='//www.syndicatestream.com/rearrange?blogID=2120213698440303768&widgetType=HTML&widgetId=HTML4&action=editWidget&sectionId=footersec1' onclick='return _WidgetManager._PopupConfig(document.getElementById("HTML4"));' target='configHTML4' title='Edit'>
+
+</a>
+</span>
+</span>
+<div class='clear'></div>
+</div></div>
+</li>
+</ul>
+</div>
+<div class='footer-widget-box'>
+<ul class='widget-container'>
+<li>
+<div class='footersec section' id='footersec2'><div class='widget HTML' id='HTML3'>
+<h2 class='title'>Blogroll</h2>
+<div class='widget-content'>
+<ul><li><a href="http://btemplates.com" title="Stream templates">BTemplates</a></li><li><a href="http://blog.btemplates.com">BTemplates Blog</a></li><li><a href="http://www.premiumStreamtemplates.com/">PBTemplates</a></li></ul>
+</div>
+<div class='clear'></div>
+<span class='widget-item-control'>
+<span class='item-control blog-admin'>
+<a class='quickedit' href='//www.syndicatestream.com/rearrange?blogID=2120213698440303768&widgetType=HTML&widgetId=HTML3&action=editWidget&sectionId=footersec2' onclick='return _WidgetManager._PopupConfig(document.getElementById("HTML3"));' target='configHTML3' title='Edit'>
+
+</a>
+</span>
+</span>
+<div class='clear'></div>
+</div></div>
+</li>
+</ul>
+</div>
+<div class='footer-widget-box footer-widget-box-last'>
+<ul class='widget-container'>
+<li>
+<div class='footersec section' id='footersec3'><div class='widget HTML' id='HTML2'>
+<h2 class='title'>About</h2>
+<div class='widget-content'>
+Mel putent quaeque an, ut postea melius denique sit. Officiis sensibus at mea, sea at labitur deserunt. Eam dicam congue soluta ut. <a href='http://btemplates.com'>Stream Templates</a>
+</div>
+<div class='clear'></div>
+<span class='widget-item-control'>
+<span class='item-control blog-admin'>
+<a class='quickedit' href='//www.syndicatestream.com/rearrange?blogID=2120213698440303768&widgetType=HTML&widgetId=HTML2&action=editWidget&sectionId=footersec3' onclick='return _WidgetManager._PopupConfig(document.getElementById("HTML2"));' target='configHTML2' title='Edit'>
+
+</a>
+</span>
+</span>
+<div class='clear'></div>
+</div></div>
+</li>
+</ul>
+</div>
+</div>
+</div>
+<div style='clear:both;'></div>
+<div id='footer-container'>
+<div id='footer'>
+<div id='copyrights'>
+Copyright &copy; <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script>
+<a href='http://syndicatestream.com/'>Syndicate Stream </a> | Powered by <a href='http://insidenothing.com/'>iN</a>
+</div>
+<div id='credits'>
+Where will we go tomorrow?
+</div>
+</div><!-- #footer -->
+</div>
+<div style='clear:both;'></div>
+</div></div></div>
+<!-- end outer-wrapper -->
+<script type="text/javascript">
+if (window.jstiming) window.jstiming.load.tick('widgetJsBefore');
+</script><script type="text/javascript" src="//www.syndicatestream.com/static/v1/widgets/1384195903-widgets.js"></script>
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+<script type='text/javascript'>
+if (typeof(BLOG_attachCsiOnload) != 'undefined' && BLOG_attachCsiOnload != null) { window['Stream_templates_experiment_id'] = "templatesV1";window['Stream_blog_id'] = '2120213698440303768';BLOG_attachCsiOnload(''); }_WidgetManager._Init('//www.syndicatestream.com/rearrange?blogID\x3d2120213698440303768','//syndicatestream.com/','2120213698440303768');
+_WidgetManager._SetDataContext([{'name': 'blog', 'data': {'blogId': '2120213698440303768', 'StreamUrl': 'http://www.syndicatestream.com', 'title': 'Syndicate Stream', 'pageType': 'index', 'url': 'http://syndicatestream.com/', 'canonicalUrl': 'http://syndicatestream.com/', 'canonicalHomepageUrl': 'http://syndicatestream.com/', 'homepageUrl': 'http://syndicatestream.com/', 'blogspotFaviconUrl': 'http://syndicatestream.com/favicon.ico', 'enabledCommentProfileImages': true, 'adultContent': false, 'disableAdSenseWidget': false, 'analyticsAccountNumber': '', 'searchLabel': '', 'searchQuery': '', 'pageName': '', 'pageTitle': 'FinanceStock', 'encoding': 'UTF-8', 'locale': 'en', 'localeLanguage': 'en', 'isPrivate': false, 'isMobile': false, 'isMobileRequest': false, 'mobileClass': '', 'isPrivateBlog': false, 'languageDirection': 'ltr', 'feedLinks': '\74link rel\75\42alternate\42 type\75\42application/atom+xml\42 title\75\42FinanceStock - Atom\42 href\75\42http://syndicatestream.com/feeds/posts/default\42 /\76\n\74link rel\75\42alternate\42 type\75\42application/rss+xml\42 title\75\42FinanceStock - RSS\42 href\75\42http://syndicatestream.com/feeds/posts/default?alt\75rss\42 /\76\n\74link rel\75\42service.post\42 type\75\42application/atom+xml\42 title\75\42FinanceStock - Atom\42 href\75\42http://syndicatestream\42 /\76\n\74link rel\75\42EditURI\42 type\75\42application/rsd+xml\42 title\75\42RSD\42 href\75\42http://www.syndicatestream.com/rsd.g?blogID\0752120213698440303768\42 /\076', 'meTag': '\74link rel\75\42me\42 href\75\42http://syndicatestream\42 /\76\n', 'openIdOpTag': '\74link rel\75\42openid.server\42 href\75\42http://www.syndicatestream.com/openid-server.g\42 /\76\n\74link rel\75\42openid.delegate\42 href\75\42http://syndicatestream.com/\42 /\76\n', 'imageSrcTag': '', 'latencyHeadScript': '\74script type\75\42text/javascript\42\76(function() { var b\75window,e\75\42jstiming\42,g\75\42tick\42;(function(){function d(a){this.t\75{};this.tick\75function(a,d,c){c\75void 0!\75c?c:(new Date).getTime();this.t[a]\75[c,d]};this[g](\42start\42,null,a)}var a\75new d;b.jstiming\75{Timer:d,load:a};if(b.performance\46\46b.performance.timing){var a\75b.performance.timing,c\75b[e].load,f\75a.navigationStart,a\75a.responseStart;0\74f\46\46a\76\75f\46\46(c[g](\42_wtsrt\42,void 0,f),c[g](\42wtsrt_\42,\42_wtsrt\42,a),c[g](\42tbsd_\42,\42wtsrt_\42))}try{a\75null,b.chrome\46\46b.chrome.csi\46\46(a\75Math.floor(b.chrome.csi().pageT),c\46\0460\74f\46\46(c[g](\42_tbnd\42,void 0,b.chrome.csi().startE),\nc[g](\42tbnd_\42,\42_tbnd\42,f))),null\75\75a\46\46b.gtbExternal\46\46(a\75b.gtbExternal.pageT()),null\75\75a\46\46b.external\46\46(a\75b.external.pageT,c\46\0460\74f\46\46(c[g](\42_tbnd\42,void 0,b.external.startE),c[g](\42tbnd_\42,\42_tbnd\42,f))),a\46\46(b[e].pt\75a)}catch(l){}})();b.tickAboveFold\75function(d){var a\0750;if(d.offsetParent){do a+\75d.offsetTop;while(d\75d.offsetParent)}d\75a;750\76\75d\46\46b[e].load[g](\42aft\42)};var h\75!1;function k(){h||(h\75!0,b[e].load[g](\42firstScrollTime\42))}b.addEventListener?b.addEventListener(\42scroll\42,k,!1):b.attachEvent(\42onscroll\42,k);\n })();\74/script\076', 'mobileHeadScript': '', 'view': '', 'dynamicViewsCommentsSrc': '//www.blogblog.com/dynamicviews/4224c15c4e7c9321/js/comments.js', 'dynamicViewsScriptSrc': '//www.blogblog.com/dynamicviews/7b32dfe08d385af5', 'plusOneApiSrc': 'https://apis.google.com/js/plusone.js', 'sf': 'n'}}, {'name': 'skin', 'data': {'vars': {'sidebartextcolor': '#666666', 'linkcolor': '#5588aa', 'visitedlinkcolor': '#999999', 'textcolor': '#333333', 'headerfont': 'normal normal 78% \47Trebuchet MS\47,Trebuchet,Arial,Verdana,Sans-serif', 'pagetitlefont': 'normal normal 200% Georgia, Serif', 'bgcolor': '#ffffff', 'descriptioncolor': '#999999', 'titlecolor': '#cc6600', 'bordercolor': '#cccccc', 'postfooterfont': 'normal normal 78% \47Trebuchet MS\47, Trebuchet, Arial, Verdana, Sans-serif', 'pagetitlecolor': '#666666', 'bodyfont': 'normal normal 100% Georgia, Serif', 'endSide': 'right', 'startSide': 'left', 'descriptionfont': 'normal normal 78% \47Trebuchet MS\47, Trebuchet, Arial, Verdana, Sans-serif', 'sidebarcolor': '#999999'}, 'override': ''}}, {'name': 'view', 'data': {'classic': {'name': 'classic', 'url': '/?view\75classic'}, 'flipcard': {'name': 'flipcard', 'url': '/?view\75flipcard'}, 'magazine': {'name': 'magazine', 'url': '/?view\75magazine'}, 'mosaic': {'name': 'mosaic', 'url': '/?view\75mosaic'}, 'sidebar': {'name': 'sidebar', 'url': '/?view\75sidebar'}, 'snapshot': {'name': 'snapshot', 'url': '/?view\75snapshot'}, 'timeslide': {'name': 'timeslide', 'url': '/?view\75timeslide'}}}]);
+_WidgetManager._RegisterWidget('_PopularPostsView', new _WidgetInfo('PopularPosts1', 'sidebartab1', null, document.getElementById('PopularPosts1'), {}, 'displayModeFull'));
+_WidgetManager._RegisterWidget('_PageListView', new _WidgetInfo('PageList8', 'pagelistmenusblog', null, document.getElementById('PageList8'), {'title': 'Pages - Menu', 'links': [{'href': 'http://syndicatestream.com/', 'title': 'Home', 'isCurrentPage': true}, {'href': 'http://syndicatestream.com/p/about.html', 'title': 'About', 'isCurrentPage': false}, {'href': 'http://syndicatestream.com/p/contact.html', 'title': 'Contact', 'isCurrentPage': false}], 'mobile': false}, 'displayModeFull'));
+_WidgetManager._RegisterWidget('_LabelView', new _WidgetInfo('Label3', 'sidebartab2', null, document.getElementById('Label3'), {}, 'displayModeFull'));
+_WidgetManager._RegisterWidget('_BlogArchiveView', new _WidgetInfo('BlogArchive1', 'sidebartab3', null, document.getElementById('BlogArchive1'), {'languageDirection': 'ltr'}, 'displayModeFull'));
+_WidgetManager._RegisterWidget('_HTMLView', new _WidgetInfo('HTML3', 'footersec2', null, document.getElementById('HTML3'), {}, 'displayModeFull'));
+_WidgetManager._RegisterWidget('_HTMLView', new _WidgetInfo('HTML2', 'footersec3', null, document.getElementById('HTML2'), {}, 'displayModeFull'));
+_WidgetManager._RegisterWidget('_HTMLView', new _WidgetInfo('HTML4', 'footersec1', null, document.getElementById('HTML4'), {}, 'displayModeFull'));
+_WidgetManager._RegisterWidget('_HTMLView', new _WidgetInfo('HTML1', 'sidebarright', null, document.getElementById('HTML1'), {}, 'displayModeFull'));
+_WidgetManager._RegisterWidget('_HTMLView', new _WidgetInfo('HTML5', 'sidebarright', null, document.getElementById('HTML5'), {}, 'displayModeFull'));
+_WidgetManager._RegisterWidget('_HTMLView', new _WidgetInfo('HTML6', 'header2', null, document.getElementById('HTML6'), {}, 'displayModeFull'));
+_WidgetManager._RegisterWidget('_HeaderView', new _WidgetInfo('Header1', 'header', null, document.getElementById('Header1'), {}, 'displayModeFull'));
+_WidgetManager._RegisterWidget('_BlogView', new _WidgetInfo('Blog1', 'main', null, document.getElementById('Blog1'), {'cmtInteractionsEnabled': false, 'lightboxEnabled': true, 'lightboxModuleUrl': '//www.syndicatestream.com/static/v1/jsbin/4050351478-lbx.js', 'lightboxCssUrl': '//www.syndicatestream.com/static/v1/v-css/228702327-lightbox_bundle.css'}, 'displayModeFull'));
+</script>
+</body>
+</html>
