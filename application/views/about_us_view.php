3542e78c00a63aab90222e876499d57822da4a6b
Patrick McGuire <insidenothing@gmail.com>
2013-05-21 20:51:20 -0400

    txt

--- application/views/about_us_view.php
+++ application/views/about_us_view.php
@@ -3,10 +3,11 @@
 		<div class="block-content">
 			<h4 class="widgettitle">At SyndicateStream,</h4>
 			<div class="widget widget_text" id="text-2">
-			<table><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we know what matters most to you, risk. 
-			With us you have a set of ears at every desk on the street.
- Our relationships and proprietary analysis give you unparalleled transparency to the syndicate calendar.
- We give you the tools to manage your risk the most efficiently. You know it as it happens. The streaming edge.<br /><br /><br /><img src="/assets/images/begin.png">
+			<table><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are able to provide you the most clear, concise, and detailed information available all in a timely manner. With over 40 years of experience in the Investment Banking industry we have built strong relationships at every major firm on the street. These relationships along with detailed comparable financial analysis enable us to give you the best chance to beat the street.
+ <br /><br /><br />
+ 
+
+From basic analysis for short term investors to trading strategies and detailed fundamental and technical analysis for long term fund managers we have you covered. <br /><br /><br /><img src="/assets/images/begin.png">
 			</td><td>
 			[login box]
 			
