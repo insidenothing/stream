5e94edcc7ad2733df7ae6a54003233c5a59a084d
Patrick McGuire <insidenothing@gmail.com>
2013-05-21 22:19:45 -0400

    fw forms

--- application/views/pricing_view.php
+++ application/views/pricing_view.php
@@ -3,7 +3,13 @@
 		<div class="block-content">
 			<h4 class="widgettitle">SyndicateStream Pricing</h4>
 			<div class="widget widget_text" id="text-2">
-			Basic: 24.99 [get html for subscription form and insert here]
+			Basic: 24.99 
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="S2KFMD6XY7X9L">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
 
  <br /><br /><br />
 
@@ -11,7 +17,13 @@ Includes timing and deal size, structure, underwriters and economics, and the Sy
 <br /><br /><br />
  
 
-Advanced: 39.99 [get html for subscription form and insert here]
+Advanced: 39.99 
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="S2KFMD6XY7X9L">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
 
  <br /><br /><br />
 
