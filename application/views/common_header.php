91fd18fc8c9db75f72cc09747b46866d5e1dc5ee
Patrick McGuire <insidenothing@gmail.com>
2013-05-21 20:17:12 -0400

    hrm

--- application/views/common_header.php
+++ application/views/common_header.php
@@ -110,7 +110,7 @@ a img{border-width:0}
 /* Outer-Wrapper----------------------------------------------- */
 #outer-wrapper{width:960px;margin:15px auto 0px;padding:0px;text-align:left;background:url(http://2.bp.blogspot.com/-OfL7G6BSZ4A/URQb0FIwdbI/AAAAAAAACIw/bRKeGl7DpE8/s000/container-bg.png) left top repeat-x;}
 #content-wrapper{background:#fff;padding:15px;}
-#main-wrapper{width:900px;float:left;margin:0px;padding:10px 10px 10px 10px;word-wrap:break-word;overflow:hidden;background:url(/assets/images/paperwork.png);background-repeat:no-repeat;}
+#main-wrapper{width:1000px;float:left;margin:0px;padding:10px 10px 10px 10px;word-wrap:break-word;overflow:hidden;background:url(/assets/images/paperwork.png);background-repeat:no-repeat;}
 #rsidebar-wrapper{width:301px;float:right;margin:0px;padding:0px 0px 0px 0px;word-wrap:break-word;overflow:hidden;}
 /* Headings----------------------------------------------- */
 h2{}
