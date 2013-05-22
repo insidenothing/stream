90c2e4cdf84441f1a64d875747edb9a374efb22f
Patrick McGuire <insidenothing@gmail.com>
2013-05-21 20:03:04 -0400

    spacing

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,12 +1,17 @@
- <div class="block" style="margin-top: 5px;">
+ <div class="block" style="margin-bottom: 15px;">
 	<div class="block-border">
 		<div class="block-content">
 			<h4 class="widgettitle">At SyndicateStream,</h4>
-			
-			<div class="widget widget_text" id="text-2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we know what matters most to you, risk. 
+			<div class="widget widget_text" id="text-2">
+			<table><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we know what matters most to you, risk. 
 			With us you have a set of ears at every desk on the street.
  Our relationships and proprietary analysis give you unparalleled transparency to the syndicate calendar.
- We give you the tools to manage your risk the most efficiently. You know it as it happens. The streaming edge. </div>
+ We give you the tools to manage your risk the most efficiently. You know it as it happens. The streaming edge. [begin button]
+			</td><td>
+			[login box]
+			
+			</td></tr></table>	
+			</div>
 		</div>
 	</div>
 </div>
