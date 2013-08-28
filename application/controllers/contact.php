f64a4f289dce4eebbea004d60c111763ac577a4b
Patrick McGuire <insidenothing@gmail.com>
2013-08-28 00:22:09 -0400

    better

--- application/controllers/contact.php
+++ application/controllers/contact.php
@@ -30,8 +30,8 @@ class Contact extends CI_Controller {
 			$this->email->bcc('syndicatestream@insidenothing.com');
 			$this->email->message($this->input->post('body'));
 			$this->email->send();
-			$data['feedback'] = "<b style='color:red;'>Message Sent...</b>";
-			$data['feedback2'] = "<b style='color:red;'>Message Sent...</b>";
+			$data['feedback'] = "<b style='color:red;'>Message Sent... We will be in contact shortly</b>";
+			$data['feedback2'] = "Your Message Was Sent...";
 		}else{
 			$data['feedback'] = "Spam Control: What is 1 + 1?";
 			$data['feedback2'] = "Send Message";
--- application/views/contact_view.php
+++ application/views/contact_view.php
@@ -27,7 +27,7 @@
 				<table width="100%">
 					<tr>
 						<td width="50%"><?php echo $feedback; ?></td>
-						<td width="50%"><select name="spam"><option>1</option><option>2</option></select></td>
+						<td width="50%"><select style="background-color:red;" name="spam"><option>1</option><option>2</option></select></td>
 					</tr>
 					<tr>
 						<td width="50%">From:</td>
