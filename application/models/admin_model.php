36de0215703946e4eaf212d8b549fdd30813a7ef
Patrick McGuire <insidenothing@gmail.com>
2013-08-28 11:20:51 -0400

    better

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -12,14 +12,16 @@ class Admin_model extends CI_Model {
 	
 	function get_list()
 	{
+		$rows = '<table width="100%">';
 		$query = $this->db->query("SELECT * from ipo_calendar order by updated_datetime DESC");
 		if ($query->num_rows() > 0)
 		{
 			foreach ($query->result() as $row)
 			{
-				$rows .= '<li>'.$row->name.' ('.$row->symbol.') '.date('l \t\h\e jS \o\f F Y',strtotime($row->published_date)).' [edit] [delete]</li>';
+				$rows .= '<tr><td>'.$row->name.'</td><td>'.$row->symbol.'</td><td>'.date('l \t\h\e jS \o\f F Y',strtotime($row->published_date)).'</td><td>[edit]</td><td>[delete]</td></tr>';
 			}
 		}
+		$rows .= '</table>';
 		return $rows;
 	}
 
