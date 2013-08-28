59822b7e36a6357cdd040a70034e3311d25810f8
Patrick McGuire <insidenothing@gmail.com>
2013-08-28 18:09:36 -0400

    theme

--- application/views/media_view.php
+++ application/views/media_view.php
@@ -1,23 +1,31 @@
-<div class="title">TV and Press</div>
-<?PHP if ($level == 'Operations'){?>
-	<div class="title">Speed in Simplicity...</div>
-	<form method="post">
-	Name:<br>
-	<input name="name" value="<?PHP echo $name;?>"><br>
-	<br>
-	Link:<br>
-	<input name="link" value="<?PHP echo $link;?>"><br>
-	<br>
-	<?PHP if ($name != ''){?>
-		<input type="submit" value="Save Update"> or <a href="/media">Discard and/or Start New</a>
-		<input type="hidden" name="query_type" value="update">
-	<?PHP }else{ ?>
-		<input type="submit" value="Save New">
-		<input type="hidden" name="query_type" value="insert">
-	<?PHP } ?>
-	</form>
-	<?PHP echo $list;?>
-	<?PHP echo $results;?>
-<?PHP } ?>
-
-<?PHP echo $public;?>
+ <div class="block" style="margin-bottom: 15px;">
+	<div class="block-border">
+		<div class="block-content">
+			<h4 class="widgettitle">TV and Press</h4>
+			<div class="widget widget_text" id="text-2">
+			<?PHP if ($level == 'Operations'){?>
+				<div class="title">Speed in Simplicity...</div>
+				<form method="post">
+				Name:<br>
+				<input name="name" value="<?PHP echo $name;?>"><br>
+				<br>
+				Link:<br>
+				<input name="link" value="<?PHP echo $link;?>"><br>
+				<br>
+				<?PHP if ($name != ''){?>
+					<input type="submit" value="Save Update"> or <a href="/media">Discard and/or Start New</a>
+					<input type="hidden" name="query_type" value="update">
+				<?PHP }else{ ?>
+					<input type="submit" value="Save New">
+					<input type="hidden" name="query_type" value="insert">
+				<?PHP } ?>
+				</form>
+				<?PHP echo $list;?>
+				<?PHP echo $results;?>
+			<?PHP } ?>
+			<?PHP echo $public;?>
+			</div>
+		</div>
+	</div>
+</div>
+ 
