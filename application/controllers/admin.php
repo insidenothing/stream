6ff80db4562f585d366d1f8862bb79e9cbebc01c
Patrick McGuire <insidenothing@gmail.com>
2013-08-28 11:12:33 -0400

    prepare admin

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -4,12 +4,18 @@ class Admin extends CI_Controller {
 
 	public function index()
 	{
-		$this->output->enable_profiler(TRUE);
-		$this->load->model('admin_model','admin');
-		$data['pages'] = $this->admin->get_pages('pages','page');
-		$data['blogs'] = $this->admin->get_pages('blogs','blog');
-		$data['wire'] = $this->admin->get_pages('wire','wire');
-		$data['ipos'] = $this->admin->get_ipos();
+		//$this->output->enable_profiler(TRUE);
+		//$this->load->model('admin_model','admin');
+		//$data['pages'] = $this->admin->get_pages('pages','page');
+		//$data['blogs'] = $this->admin->get_pages('blogs','blog');
+		//$data['wire'] = $this->admin->get_pages('wire','wire');
+		//$data['ipos'] = $this->admin->get_ipos();
+		
+		
+		
+		
+		
+		
 		$this->load->library('Menu','menu');
 		$this->menu->load_common('admin_view',$data);
 	}
---
+++ application/views/admin_view.php
@@ -0,0 +1,5 @@
+<div class="title">Speed in Simplicity...</div>
+<div class="info">+Create New Release</div>
+<div class="info">+Delete Release</div>
+<div class="info">+Edit Release</div> 
+
