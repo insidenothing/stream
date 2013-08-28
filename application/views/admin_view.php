0690deeb0d48be4243984a3f17f9fcdd058802a5
Patrick McGuire <insidenothing@gmail.com>
2013-08-28 11:39:21 -0400

    name elements

--- application/views/admin_view.php
+++ application/views/admin_view.php
@@ -2,42 +2,35 @@
 <table><tr><td>
 <table>
 	<tr>
-		<td>name</td>
 		<td>Company Name</td>
-		<td><input></td>
+		<td><input name="name"></td>
 	</tr>
 	<tr>
-		<td>symbol</td>
 		<td>Symbol</td>
-		<td><input></td>
+		<td><input name="symbol"></td>
 	</tr>
 	<tr>
-		<td>managers</td>
 		<td>Managers</td>
-		<td><input></td>
+		<td><input name="managers"></td>
 	</tr>
 	<tr>
-		<td>catagory</td>
 		<td>Type</td>
-		<td><input></td>
+		<td><input name="catagory"></td>
 	</tr>
 	<tr>
-		<td>shares</td>
 		<td>Shares</td>
-		<td><input>M</td>
+		<td><input name="shares">M</td>
 	</tr>
 	<tr>
-		<td>price</td>
 		<td>Priced at</td>
-		<td>$<input></td>
+		<td>$<input name="price"></td>
 	</tr>
 </table>
 </td><td>
 <table>
 	<tr>
-		<td>updates</td>
 		<td>Update Notes</td>
-		<td><textarea cols="75" rows="15"></textarea></td>
+		<td><textarea name="updates" cols="75" rows="15"></textarea></td>
 	</tr>
 </table>
 </td></tr>
